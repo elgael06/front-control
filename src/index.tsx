b91184cddb7d73ebbe431388e3a7e730748ab6fb
gael <cgvalenzuelac@concredito.com.mx>
2021-04-27 15:22:59 -0600

    mescaldo

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
@@ -10,14 +8,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store} >
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store} >
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 defineCustomElements(window);
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
-reportWebVitals();
+// reportWebVitals();
